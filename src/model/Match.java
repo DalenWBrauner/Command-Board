58fed77097d37b0946f5dc4cd605e27632b8b5b6
Dalen W. Brauner <Dalen.Brauner12@ncf.edu>
2015-05-08 01:18:46 -0400

    Moved an old debug function into a separate Test object

--- src/model/Match.java
+++ src/model/Match.java
@@ -104,6 +104,8 @@ public class Match extends Observable {
         boolean winCondition = false;
 
         // SpellCasting
+        // TODO Replace this section with a call to a SpellCaster object
+        // e.g. SpellCaster.prepare(currentPlayer);
         // First, determine the spells the player can cast.
         SpellID[] castableSpells = getPlayer(currentPlayer).getHand().getCastableSpells();
 
@@ -172,56 +174,6 @@ public class Match extends Observable {
         } else {
             return PlayerID.NOPLAYER;
         }
-    }
-
-    public void debug() {
-        assert(players.size() == 4);
-        System.out.println("Match.debug(); START");
-        printBoard();
-
-        Tile tileA = theBoard.getTile(2,2);
-        if (tileA.getTileType() == TileType.PROPERTY) {
-            ((PropertyTile) tileA).setCard(CardShape.SHAPE1);
-            players.get(0).gainTile((PropertyTile) tileA);
-        } else { assert(false); }
-        System.out.println("Player 1 now owns the tile at (2,2)...");
-
-        Tile tileB = theBoard.getTile(3,3);
-        if (tileB.getTileType() == TileType.PROPERTY) {
-            players.get(1).gainTile((PropertyTile) tileB);
-            ((PropertyTile) tileB).setCard(CardShape.SHAPE2);
-        } else { assert(false); }
-        System.out.println("Player 2 now owns the tile at (3,3)...");
-
-        Tile tileC = theBoard.getTile(5,4);
-        if (tileC.getTileType() == TileType.PROPERTY) {
-            players.get(2).gainTile((PropertyTile) tileC);
-            ((PropertyTile) tileC).setCard(CardShape.SHAPE3);
-        } else { assert(false); }
-        System.out.println("Player 3 now owns the tile at (5,4)...");
-
-        Tile tileD = theBoard.getTile(2,7);
-        if (tileD.getTileType() == TileType.PROPERTY) {
-            players.get(3).gainTile((PropertyTile) tileD);
-            ((PropertyTile) tileD).setCard(CardShape.SHAPE1);
-        } else { assert(false); }
-        System.out.println("Player 4 now owns the tile at (2,7)...");
-        System.out.println();
-
-        printBoard();
-
-        ((PropertyTile) tileA).setLevel(2);
-        System.out.println("Player 1 has leveled up their tile by 1!");
-        ((PropertyTile) tileB).setLevel(3);
-        System.out.println("Player 2 has leveled up their tile by 2!");
-        ((PropertyTile) tileC).setLevel(4);
-        System.out.println("Player 3 has leveled up their tile by 3!");
-        ((PropertyTile) tileD).setLevel(5);
-        System.out.println("Player 4 has leveled up their tile by 4!");
-        System.out.println();
-
-        printBoard();
-        System.out.println("Match.debug(); END");
     }
 
     /** Prints out basic information about the current board. */

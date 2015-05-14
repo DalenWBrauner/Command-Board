cb260ec7ea3127e7861e043ab2c3b6361be049b6
Dalen W. Brauner <Dalen.Brauner12@ncf.edu>
2015-05-14 16:03:53 -0400

    Added names to Cardshapes

--- src/shared/enums/CardShape.java
+++ src/shared/enums/CardShape.java
@@ -1,3 +1,21 @@
 package shared.enums;
 
-public enum CardShape { NOCARD, SHAPE1, SHAPE2, SHAPE3 }
+public enum CardShape {
+
+    NOCARD ("None"),
+    SHAPE1 ("Circle"),
+    SHAPE2 ("Square"),
+    SHAPE3 ("Triangle");
+
+    private final String myName;
+    private CardShape(String name) {
+        myName = name;
+    }
+
+    @Override
+    public String toString() {
+        return myName;
+    }
+
+}
+
